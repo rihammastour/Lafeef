745ad8ee08ebf907bc7b7ca4bd1bd769c0a084e8
renadnasser1 <56361699+renadnasser1@users.noreply.github.com>
2021-01-26 21:01:32 +0300

    Helper classes

---
+++ Lafeef/Helper/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  LoginDemo
+//
+//  Created by Renad nasser on 26/01/2021.
+//  Copyright © 2021 Renad Nasser. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        static let homeViewController = "HomeVC"
+    }
+}
