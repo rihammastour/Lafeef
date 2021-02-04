78691b8ef26db06ba180a525e7c329229bb4294d
raghad <raghadalfheed76@gmail.com>
2021-02-04 17:43:22 +0300

    Update UserInterfaceState.xcuserstate

--- Pods/Target Support Files/Pods-Lafeef-LafeefUITests/Pods-Lafeef-LafeefUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Lafeef_LafeefUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Lafeef_LafeefUITests
-@end
--- Pods/Target Support Files/Pods-Lafeef/Pods-Lafeef-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Lafeef : NSObject
-@end
-@implementation PodsDummy_Pods_Lafeef
-@end
