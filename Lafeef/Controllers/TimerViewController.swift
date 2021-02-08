23779f605fc317c779ec5a38e6f09a914c08acd3
raghad <raghadalfheed76@gmail.com>
2021-02-08 21:43:27 +0300

    front-End Timer

--- Lafeef/Controllers/TimerViewController.swift
+++ Lafeef/Controllers/TimerViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 class TimerViewController: UIViewController {
     let timeLeftShapeLayer = CAShapeLayer()
     let bgShapeLayer = CAShapeLayer()
-    var timeLeft: TimeInterval = 60
+    var timeLeft: TimeInterval = 18//change
+    let timeLeft1=18//change
     var endTime: Date?
     var timeLabel =  UILabel()
     var timer = Timer()
@@ -20,7 +21,7 @@ class TimerViewController: UIViewController {
     
     func drawBgShape() {
         bgShapeLayer.path = UIBezierPath(arcCenter: CGPoint(x: view.frame.midX , y: view.frame.midY), radius:
-            100, startAngle: -90.degreesToRadians, endAngle: 270.degreesToRadians, clockwise: true).cgPath
+            55, startAngle: -90.degreesToRadians, endAngle: 270.degreesToRadians, clockwise: true).cgPath
 //        bgShapeLayer.strokeColor = UIColor.white.cgColor
         bgShapeLayer.fillColor = UIColor.clear.cgColor
         bgShapeLayer.lineWidth = 15
@@ -29,20 +30,22 @@ class TimerViewController: UIViewController {
     
     
     func drawTimeLeftShape() {
-        timeLeftShapeLayer.path = UIBezierPath(arcCenter: CGPoint(x: view.frame.midX , y: view.frame.midY), radius:
-            100, startAngle: -90.degreesToRadians, endAngle: 270.degreesToRadians, clockwise: true).cgPath
+        timeLeftShapeLayer.path = UIBezierPath(arcCenter: CGPoint(x: view.frame.midX - 100, y: view.frame.midY-100 ), radius:
+            30, startAngle: -90.degreesToRadians, endAngle: 270.degreesToRadians, clockwise: true).cgPath
         timeLeftShapeLayer.strokeColor = UIColor(hue: 0.1222, saturation: 0.46, brightness: 0.94, alpha: 1.0).cgColor
         timeLeftShapeLayer.fillColor = UIColor.clear.cgColor
-        timeLeftShapeLayer.lineWidth = 150
+        //change the circle size
+        timeLeftShapeLayer.lineWidth = 60
         view.layer.addSublayer(timeLeftShapeLayer)
     }
     
     
     func addTimeLabel() {
-        timeLabel = UILabel(frame: CGRect(x: view.frame.midX-50 ,y: view.frame.midY-25, width: 100, height: 50))
+        timeLabel = UILabel(frame: CGRect(x: view.frame.midX-150 ,y: view.frame.midY-125, width: 100, height: 50))
         timeLabel.textAlignment = .center
         timeLabel.text = timeLeft.time
         view.addSubview(timeLabel)
+//        print(timeLeft.time)
     }
     
     
@@ -67,7 +70,7 @@ class TimerViewController: UIViewController {
            view.backgroundColor = UIColor(white: 0.94, alpha: 1.0)
            drawBgShape()
            drawTimeLeftShape()
-           addTimeLabel()
+//           addTimeLabel()
            // here you define the fromValue, toValue and duration of your animation
 //           strokeIt.fromValue = 0
 //           strokeIt.toValue = 1
@@ -90,11 +93,38 @@ class TimerViewController: UIViewController {
     }
     
     @objc func updateTime() {
-    if timeLeft > 0 {
+//        let greenTime=timeLeft+1//18
+       
+        print(timeLeft1)
+        let greenTime=timeLeft1/3*2//12
+        
+        let yellowTime=timeLeft1/3*1//6
+        
+        
+        if(Int(timeLeft)>greenTime){
         timeLeft = endTime?.timeIntervalSinceNow ?? 0
         timeLabel.text = timeLeft.time
-        } else {
-        timeLabel.text = "00:00"
+        //green
+        timeLeftShapeLayer.strokeColor = UIColor(hue: 0.1861, saturation: 0.36, brightness: 0.88, alpha: 1.0).cgColor
+       
+    }
+        else  if (Int(timeLeft) > yellowTime){
+     timeLeft = endTime?.timeIntervalSinceNow ?? 0
+     timeLabel.text = timeLeft.time
+     //yellow
+        timeLeftShapeLayer.strokeColor = UIColor(hue: 0.1333, saturation: 0.86, brightness: 1, alpha: 1.0).cgColor
+     }
+    
+       else  if (timeLeft > 0 ){
+        timeLeft = endTime?.timeIntervalSinceNow ?? 0
+        timeLabel.text = timeLeft.time
+        //orange
+        timeLeftShapeLayer.strokeColor = UIColor(hue: 0.0972, saturation: 0.55, brightness: 0.91, alpha: 1.0).cgColor
+        }
+    else {
+        timeLabel.text = "انتهى الوقت!"
+        //red
+            timeLeftShapeLayer.strokeColor = UIColor(hue: 0, saturation: 0.5, brightness: 0.95, alpha: 1.0).cgColor
         timer.invalidate()
         }
     }
