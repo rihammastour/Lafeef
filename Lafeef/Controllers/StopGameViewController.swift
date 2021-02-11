23e1ac4f944e87602b85fa860c689012452325d8
raghad <raghadalfheed76@gmail.com>
2021-02-11 22:49:15 +0300

    .

---
+++ Lafeef/Controllers/StopGameViewController.swift
@@ -0,0 +1,15 @@
+//
+//  stopGameViewController.swift
+//  Lafeef
+//
+//  Created by Raghad alfuhaid on 29/06/1442 AH.
+//
+
+import Foundation
+import UIKit
+class StopGameViewController: UIViewController{
+    
+    @IBOutlet weak var Complete: UIButton!
+    
+    
+}
