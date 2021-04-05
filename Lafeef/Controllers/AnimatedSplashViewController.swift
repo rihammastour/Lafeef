b48e631ea2bd351f4135c06b3ce27baa3bcdcc1a
Mihaf AJunaidel <maljunaydil@taqeem.gov.sa>
2021-04-05 15:43:07 +0300

    Revert "add"
    This reverts commit c6ea801829ef40aefcb389eac548660777106168.

--- Lafeef/Controllers/AnimatedSplashViewController.swift
+++ Lafeef/Controllers/AnimatedSplashViewController.swift
@@ -62,72 +62,8 @@ class AnimatedSplashViewController: UIViewController, SwiftyGifDelegate {
             UserDefaults.standard.set(true, forKey: "launchedBefore")
         }
     }
-    func getChildPrefrence(){
-        //Get Child Prefrences
-        FirebaseRequest.getChildEquipments { (data, err) in
-            if data != nil {
-                print("Data ",data)
-                do{
-                    let equipments = try FirebaseDecoder().decode([ChildEquipment].self, from: data)
-                    self.childEquipment = equipments
-                  
-                }catch{
-                    print("Incorrect Format")
-                }
-            }else{
-                print("error")
-            }
-        }
-        print("dddddddd ",childEquipment)
-   
-    }
 
   
-    func reflecBackerytUserEquipment(equipmentName:String){
-        print("inside reflect")
-        for item in self.childEquipment{
-            if !item.inUse && item.name.contains("Frame"){
-                print("frame")
-                GameScene.presentBackeyEquipment(at: item.name)
-                FirebaseRequest.updateUserEquipment(equipmentName: item.name) { (update, error) in
-                        if error == nil {
-                            print("updated")
-                        }else{
-                            print("eror")
-                        }
-                    }
-                    
-            }else if !item.inUse && item.name == "lamp"{
-                print("lamp")
-                GameScene.presentBackeyEquipment(at: item.name)
-                FirebaseRequest.updateUserEquipment(equipmentName:item.name) { (update, error) in
-                        if error == nil {
-                            print("updated")
-                        }else{
-                            print("eror")
-                        }
-                    }
-            }
-        }
-        }
-    func reflecCharachtertUserEquipment(equipmentName:String){
-        print("inside reflect")
-        for item in self.childEquipment{
-            if !item.inUse && item.name == equipmentName{
-                print("frame")
-           
-//                HomeViewController.setImage(<#String#>)
-                FirebaseRequest.updateUserEquipment(equipmentName: item.name) { (update, error) in
-                        if error == nil {
-                            print("updated")
-                        }else{
-                            print("eror")
-                        }
-                    }
-
-            }
-        }
     
-        }
     
 }
