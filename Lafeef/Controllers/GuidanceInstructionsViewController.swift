052decc36651936ce5d033d845b0964b0f0bb69c
renadnasser1 <56361699+renadnasser1@users.noreply.github.com>
2021-05-02 00:17:50 +0300

    enhance instructions

--- Lafeef/Controllers/GuidanceInstructionsViewController.swift
+++ Lafeef/Controllers/GuidanceInstructionsViewController.swift
@@ -17,20 +17,19 @@ class GuidanceInstructionsViewController: UIViewController {
     @IBOutlet weak var instructionView: UIView!
     @IBOutlet weak var reflector: UIImageView!
     @IBOutlet weak var nextlOutlet: UIButton!
-    @IBOutlet weak var skiplOutlet: UIButton!
     @IBOutlet weak var doneOutlet: UIButton!
+    @IBOutlet weak var backOutlet: UIButton!
+    @IBOutlet weak var instructionNumLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         nextlOutlet.layer.cornerRadius =  nextlOutlet.frame.size.height/2
         
-        skiplOutlet?.layer.cornerRadius =  skiplOutlet.frame.size.height/2
+        backOutlet?.layer.cornerRadius =  backOutlet.frame.size.height/2
         
-        doneOutlet?.layer.cornerRadius =  skiplOutlet.frame.size.height/2
+        doneOutlet?.layer.cornerRadius =  doneOutlet.frame.size.height/2
         
-        instructionView.layer.cornerRadius = 30
-        reflector.image = UIImage.gif(name: instructionData[instructionIndex])
-//        icecream.image = UIImage.gif(name: "Stand")
+        showInstruction()
         let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
                 backgroundImage.image = UIImage(named: "blank-bakery")
                 backgroundImage.contentMode = UIView.ContentMode.scaleToFill
@@ -39,29 +38,61 @@ class GuidanceInstructionsViewController: UIViewController {
                 view.backgroundColor = UIColor(white: 1, alpha: 0.5)
     }
     
-    func showNextInstruction(){
+    func showInstruction(){
         instructionView.layer.cornerRadius = 30
         reflector.image = UIImage.gif(name: instructionData[instructionIndex])
+        updatLabel()
     }
     
-    func changeButton(){
+    func lastInstructionButtons(){
         self.nextlOutlet.isHidden = true
-        self.skiplOutlet.isHidden = true
         self.doneOutlet.isHidden = false
+        self.backOutlet.isHidden = false
     }
+    
+    func middileInstructionButtons(){
+        self.nextlOutlet.isHidden = false
+        self.doneOutlet.isHidden = true
+        self.backOutlet.isHidden = false
+    }
+    
+    func firstInstructionButtons(){
+        self.nextlOutlet.isHidden = false
+        self.doneOutlet.isHidden = true
+        self.backOutlet.isHidden = true
+    }
+    func updatLabel(){
+        instructionNumLabel.text = "\(instructionData.count) \\ \(instructionIndex+1)"
+    }
+    
     
     @IBAction func nextInstruction(_ sender: Any) {
         instructionIndex+=1
         if instructionIndex < instructionData.count{
-            showNextInstruction()
+            showInstruction()
             if instructionIndex == instructionData.count-1{
-                changeButton()
+                lastInstructionButtons()
+            }else{
+                middileInstructionButtons()
             }
         }
     }
     
     @IBAction func skipInstructionsTapped(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func goBackTapped(_ sender: Any) {
+        instructionIndex-=1
+        if instructionIndex >= 0{
+            showInstruction()
+            if instructionIndex == 0{
+                middileInstructionButtons()
+            }else{
+                firstInstructionButtons()
+            }
+        }
+        
     }
     @IBAction func doneTapped(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
